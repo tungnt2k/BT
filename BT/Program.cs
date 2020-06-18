880ff2397b9f77d235cb2d58eb2dcdbd8bceb09b
tungnt2k <tuta1w1aa0000@gmail.com>
2020-06-18 07:38:20 +0700

    Add project files.

---
+++ BT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace BT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            TComplex sp1 = new TComplex(3,4);
+            TComplex sp2 = new TComplex(1,2);
+            TComplex sp3 = new TComplex(5,6);
+            sp1.Xuat();
+            sp2.Xuat();
+            sp3.Xuat();
+            Console.WriteLine("Nhập dữ liệu cho số phức 1");
+            sp1.Nhap();
+            Console.WriteLine("Nhập dữ liệu cho số phức 2");
+            sp2.Nhap();
+            Console.WriteLine("Nhập dữ liệu cho số phức 3");
+            sp3.Nhap();
+            sp1.Xuat();
+            sp2.Xuat();
+            sp3.Xuat();
+
+            sp3 = sp1 - sp2;
+            sp3.Xuat();
+            TComplex sp4 = sp1 + sp2;
+            sp4.Xuat();
+            TComplex sp5 = sp4 * sp3;
+            sp5.Xuat();
+
+
+        }
+    }
+}
---
+++ BT/TComplex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT
+{
+    class TComplex
+    {
+        public int pThuc { get; set; }
+        public int pAo { get; set; }
+
+        public TComplex()
+        {
+            this.pAo = 0;
+            this.pThuc=0;
+        }
+        public TComplex(int pt, int pa)
+        {
+            this.pThuc = pt;
+            this.pAo = pa;
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhập phần thực : ");
+            this.pThuc = int.Parse(Console.ReadLine());
+            Console.Write("Nhập phần ảo : ");
+            this.pAo = int.Parse(Console.ReadLine());
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("Số thực là : {0} + {1}i",this.pThuc,this.pAo);
+        }
+
+        public static TComplex operator +(TComplex sp1, TComplex sp2)
+        {
+            TComplex sp = new TComplex(sp1.pThuc+sp2.pThuc,sp1.pAo+sp2.pAo);
+            return sp;
+        }
+
+        public static TComplex operator -(TComplex sp1, TComplex sp2)
+        {
+            TComplex sp = new TComplex(sp1.pThuc - sp2.pThuc, sp1.pAo - sp2.pAo);
+            return sp;
+        }
+        public static TComplex operator *(TComplex sp1, TComplex sp2)
+        {
+            TComplex sp = new TComplex((sp1.pThuc * sp2.pThuc) -(sp1.pAo * sp2.pAo), (sp1.pThuc * sp2.pAo) + (sp1.pAo * sp2.pThuc));
+            return sp;
+        }
+    }
+}
